56e93374df977661ea0b5e2823f064d92c50b730
bobur <valiyev7115@gmail.com>
2025-02-12 08:40:39 +0500

    Palimarfizim2

---
+++ basket 1.py
@@ -0,0 +1,43 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def info(self):
+        return f"Mahsulot, {self.name}, Narx, {self.price}, {self.quantity}"
+
+    def sell(self, amount):
+        if amount > self.quantity:
+            print(f"Bizda {self.quantity} ta mahsulot bor xolos")
+        else:
+            self.quantity -= amount
+
+    def restock(self, amount):
+        self.quantity += amount
+
+
+class Electronics(Product):
+    def __init__(self, name, price, quantity, warranty):
+        super().__init__(name, price, quantity)
+        self.warranty = warranty
+
+    def info(self):
+        data = super().info()
+        data += f" {self.warranty}"
+        return data
+
+
+class Food(Product):
+    def __init__(self, name, price, quantity, sirok):
+        super().__init__(name, price, quantity, )
+        self.sirok = sirok
+
+    def info(self):
+        data = super().info()
+        data += f" || sirok :{self.sirok}"
+        return data
+
+
+e = Electronics("Apple", 12, 12, "1 yil", )
+print(e.info())
---
+++ main.py
@@ -0,0 +1,24 @@
+# class Transport:
+#     def __init__(self, name, speed):
+#         self.name = name
+#         self.speed = speed
+#
+#     def info(self):
+#         print(f"Nomi:, {self.name}, {self.speed}")
+#
+#
+# class Car(Transport):
+#     def info(self):
+#         print("Mashina yurmoqda")
+#
+#
+# class Bicycle(Transport):
+#     def info(self):
+#         print("Velosapet yurmoqda")
+#
+# s = Car("BMW", "100 tezlik")
+# s.info()
+#
+# v = Bicycle("Gonchik", "10 tezlik")
+#
+# v.info()
